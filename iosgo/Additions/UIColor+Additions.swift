4eb02d54b22cd71e9e4dd9ec915f9c9b45521bc3
Jeff Woo <cheeseonhead@gmail.com>
2017-02-17 01:23:58 -0500

    Copied methods from the internet

--- iosgo/Additions/UIColor+Additions.swift
+++ iosgo/Additions/UIColor+Additions.swift
@@ -8,6 +8,20 @@ import UIKit
 
 extension UIColor
 {
+    // Usage: UIColor(hex: 0xFC0ACE)
+    convenience init(hex: Int) {
+        self.init(hex: hex, alpha: 1)
+    }
+
+    // Usage: UIColor(hex: 0xFC0ACE, alpha: 0.25)
+    convenience init(hex: Int, alpha: Double) {
+        self.init(
+                red: CGFloat((hex >> 16) & 0xff) / 255,
+                green: CGFloat((hex >> 8) & 0xff) / 255,
+                blue: CGFloat(hex & 0xff) / 255,
+                alpha: CGFloat(alpha))
+    }
+
     func lighterColor() -> UIColor
     {
         var h: CGFloat = 0.0
