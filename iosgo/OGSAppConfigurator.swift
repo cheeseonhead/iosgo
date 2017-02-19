cb2eca7b7fc900166d426462090360909ccbed71
Jeffrey Wu <jeffrey@tophat.com>
2017-02-19 02:57:15 -0500

    Added a bunch of files

---
+++ iosgo/OGSAppConfigurator.swift
@@ -0,0 +1,9 @@
+//
+// Created by Jeffrey Wu on 2017-02-18.
+// Copyright (c) 2017 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
+
+class OGSAppConfigurator {
+}
---
+++ iosgo/Stores/OGSStoreStatusCode.swift
@@ -0,0 +1,6 @@
+//
+// Created by Jeffrey Wu on 2017-02-17.
+// Copyright (c) 2017 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
