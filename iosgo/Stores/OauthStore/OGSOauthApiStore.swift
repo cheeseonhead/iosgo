f5040b62c2ea45984edc8229135fccabb57c4722
Jeffrey Wu <jeffrey@tophat.com>
2017-02-22 15:25:44 -0500

    Implemented the class to pass back the correct result depending on the response from web

--- iosgo/Stores/OauthStore/OGSOauthApiStore.swift
+++ iosgo/Stores/OauthStore/OGSOauthApiStore.swift
@@ -7,7 +7,7 @@ import Foundation
 
 class OGSOauthApiStore
 {
-    func login(with username: String, password: String)
+    func getToken(with username: String, password: String, completion: @escaping (OGSOauthStore.GetTokenResult) -> Void)
     {
         var url = "oauth2/token/"
 
@@ -20,8 +20,38 @@ class OGSOauthApiStore
         ]
 
         OGSApiManager.sharedInstance.request(toUrl: url, method: .POST, parameters: params)
-        { _, _, _ in
+        { statusCode, payload, _ in
 
+            switch statusCode
+            {
+            case .ok:
+                guard let correctPayload = payload as? [String: String],
+                    let tokenInfo = self.createTokenInfo(from: correctPayload) else
+                {
+                    completion(.error(type: .unknownError))
+                    return
+                }
+                completion(.success(info: tokenInfo))
+            case .unauthorized:
+                completion(.error(type: .invalidLoginInfo))
+                break
+            default:
+                completion(.error(type: .unknownError))
+            }
         }
+    }
+
+    fileprivate func createTokenInfo(from payload: [String: String]) -> OGSOauthStore.TokenInfo?
+    {
+        guard let accessToken = payload["access_token"],
+            let tokenType = payload["token_type"],
+            let expiresIn = payload["expires_in"],
+            let refreshToken = payload["refresh_token"],
+            let scope = payload["scope"] else
+        {
+            return nil
+        }
+
+        return OGSOauthStore.TokenInfo(accessToken: accessToken, tokenType: tokenType, expiresIn: expiresIn, refreshToken: refreshToken, scope: scope)
     }
 }
