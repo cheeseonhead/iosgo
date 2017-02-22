38ccdcb62c152251fdb337b6177e57be0aed3682
Jeffrey Wu <jeffrey@tophat.com>
2017-02-21 21:52:26 -0500

    Finished disk manager to handle all those things

--- iosgo/OGSDiskManager.swift
+++ iosgo/OGSDiskManager.swift
@@ -5,7 +5,7 @@
 
 import Foundation
 
-protocol OGSCoding: NSCoding
+protocol OGSVersionedCoding: NSCoding
 {
     static var DataName: String { get }
     static var DataVersion: Int { get }
@@ -13,21 +13,68 @@ protocol OGSCoding: NSCoding
 
 class OGSDiskManager
 {
-    static var docPath: String = {
-        var paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
-        return paths[0]
-    }()
+    static let DataFileExtension = ".plist"
 
-    func absoluteFileName(_ fileName: String) -> String
+    enum DataKey: String
     {
-        return type(of: self).docPath.appending(fileName)
+        case version
+        case payload
     }
 
-    func saveData(_ data: Any, objectName: String)
-    {
-        var dataPath = absoluteFileName(objectName)
-        var objectData = NSArray.init(object: data)
+    static var docPath: URL = {
+        var paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        return URL.init(string: paths[0])!
+    }()
 
-        NSKeyedArchiver.archiveRootObject(objectData, toFile: dataPath)
+    static func saveObject<PayloadClass: NSObject>(_ objectToStore: PayloadClass) -> Bool where PayloadClass: OGSVersionedCoding
+    {
+        var dataPath = absoluteFileName(type(of: objectToStore).DataName)
+        var objectData: [String: Any] = [
+            stringFor(dataKey: .version): type(of: objectToStore).DataVersion,
+            stringFor(dataKey: .payload): objectToStore,
+        ]
+
+        var success = NSKeyedArchiver.archiveRootObject(objectData, toFile: dataPath)
+        return success
+    }
+
+    static func getData<PayloadClass: OGSVersionedCoding>(forClass payLoadClass: PayloadClass.Type) -> PayloadClass?
+    {
+        var dataPath = absoluteFileName(payLoadClass.DataName)
+
+        do
+        {
+            guard let objectData = try NSKeyedUnarchiver.unarchiveObject(withFile: dataPath) as? [String: Any],
+                let storedVersion = objectData[stringFor(dataKey: .version)] as? Int,
+                let payload = objectData[stringFor(dataKey: .payload)] as? PayloadClass else
+            {
+                return nil
+            }
+
+            guard storedVersion == payLoadClass.DataVersion else
+            {
+                return nil
+            }
+
+            return payload
+        }
+        catch
+        {
+            return nil
+        }
+    }
+}
+
+// MARK: - Helpers
+fileprivate extension OGSDiskManager
+{
+    static func absoluteFileName(_ fileName: String) -> String
+    {
+        return docPath.appendingPathComponent(fileName).absoluteString.appending(DataFileExtension)
+    }
+
+    static func stringFor(dataKey: DataKey) -> String
+    {
+        return dataKey.rawValue
     }
 }
