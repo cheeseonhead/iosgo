504a43fbf6d59bde43e8cd2ced0fb4d63c60b479
Jeffrey Wu <jeffrey@tophat.com>
2017-02-22 01:44:41 -0500

    Made the apistore call the method to the url

--- iosgo/Stores/OGSOauthApiStore.swift
+++ iosgo/Stores/OGSOauthApiStore.swift
@@ -5,5 +5,23 @@
 
 import Foundation
 
-class OGSOauthApiStore {
+class OGSOauthApiStore
+{
+    func login(with username: String, password: String)
+    {
+        var url = "oauth2/token/"
+
+        var params = [
+            "client_id": OGSApiManager.sharedInstance.clientId!,
+            "client_secret": OGSApiManager.sharedInstance.clientSecret!,
+            "grant_type": "password",
+            "username": username,
+            "password": password,
+        ]
+
+        OGSApiManager.sharedInstance.request(toUrl: url, method: .POST, parameters: params)
+        { _, _, _ in
+
+        }
+    }
 }
