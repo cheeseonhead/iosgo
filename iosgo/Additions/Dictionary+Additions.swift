1b67c34c76b085581b80803b04d8c2cc3e38281e
Jeff Woo <cheeseonhead@gmail.com>
2017-02-24 01:19:42 -0500

    Moved the extensions into their own files

---
+++ iosgo/Additions/Dictionary+Additions.swift
@@ -0,0 +1,29 @@
+//
+// Created by Cheese Onhead on 2/24/17.
+// Copyright (c) 2017 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
+
+extension Dictionary
+{
+    /// Build string representation of HTTP parameter dictionary of keys and objects
+    ///
+    /// This percent escapes in compliance with RFC 3986
+    ///
+    /// http://www.ietf.org/rfc/rfc3986.txt
+    ///
+    /// :returns: String representation in the form of key1=value1&key2=value2 where the keys and values are percent escaped
+
+    func stringFromHttpParameters() -> String
+    {
+        let parameterArray = self.map
+        { (key, value) -> String in
+            let percentEscapedKey = (key as! String).addingPercentEncodingForURLQueryValue()!
+            let percentEscapedValue = (value as! String).addingPercentEncodingForURLQueryValue()!
+            return "\(percentEscapedKey)=\(percentEscapedValue)"
+        }
+
+        return parameterArray.joined(separator: "&")
+    }
+}
---
+++ iosgo/Additions/String+Additions.swift
@@ -0,0 +1,23 @@
+//
+// Created by Cheese Onhead on 2/24/17.
+// Copyright (c) 2017 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
+
+extension String
+{
+    /// Percent escapes values to be added to a URL query as specified in RFC 3986
+    ///
+    /// This percent-escapes all characters besides the alphanumeric character set and "-", ".", "_", and "~".
+    ///
+    /// http://www.ietf.org/rfc/rfc3986.txt
+    ///
+    /// :returns: Returns percent-escaped string.
+
+    func addingPercentEncodingForURLQueryValue() -> String? {
+        let allowedCharacters = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~")
+
+        return self.addingPercentEncoding(withAllowedCharacters: allowedCharacters)
+    }
+}
