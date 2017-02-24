0b4a71a6cdb1297f9c50632f9fcb96c9e0880db7
Jeff Woo <cheeseonhead@gmail.com>
2017-02-24 01:04:03 -0500

    Added method in presenter

--- iosgo/Scenes/Login/OGSLoginPresenter.swift
+++ iosgo/Scenes/Login/OGSLoginPresenter.swift
@@ -14,6 +14,7 @@ import UIKit
 protocol OGSLoginPresenterInput
 {
     func presentLogin(response: OGSLogin.Login.Response)
+    func presentFieldsChange(response: OGSLogin.FieldsChanged.Response)
 }
 
 protocol OGSLoginPresenterOutput: class
@@ -37,6 +38,11 @@ class OGSLoginPresenter: OGSLoginPresenterInput
         OGSDispatcher.asyncMain {
             self.output.displayLogin(viewModel: viewModel)
         }
+    }
+
+    func presentFieldsChange(response: OGSLogin.FieldsChanged.Response)
+    {
+
     }
 }
 
