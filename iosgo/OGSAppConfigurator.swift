41c86e47794ef9ffa98434c62372ac716fbd1a04
Jeffrey Wu <jeffrey@tophat.com>
2017-02-24 00:03:14 -0500

    Added the configuration class

--- iosgo/OGSAppConfigurator.swift
+++ iosgo/OGSAppConfigurator.swift
@@ -42,9 +42,16 @@ fileprivate extension OGSAppConfigurator
         OGSDiskManager.saveObject(userSetting)
     }
 }
+
+fileprivate class Configuration
+{
+    static var DataName: String = "Configuration"
+    static var DataVersion: Int = 1
+
     var domainName = "https://beta.online-go.com/"
     var clientID = "T0upLgPLIiuDt1zozbgEAqt1Cho3wWDRc2Iw6iJQ"
     var clientSecret = "6YMRzrSCoKYvu5iTxH6cHl8WZUsr5G8pHghsYxc03rHOSsrLSk2fAzPwLvrOItYwixKE1elPAiAmrLH9vkftKCK3KNGxpME67MxazWrIMJ2kE08Yc2foyIcH3IYnfjhk"
+}
 
     func configureApp()
     {
