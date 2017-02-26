83be1d9e740d21f8b4dbf45ccda58525dfb8ce1b
Jeffrey Wu <jeffrey@tophat.com>
2017-02-26 01:47:37 -0500

    Implemented the methods for UserSettingsStore

--- iosgo/Stores/UserSettingsStore/OGSUserSettingsStore.swift
+++ iosgo/Stores/UserSettingsStore/OGSUserSettingsStore.swift
@@ -7,4 +7,48 @@ import Foundation
 
 class OGSUserSettingsStore: OGSUserSettingsStoreProtocol
 {
+    fileprivate var userSettings: OGSUserSettings!
+
+    func save(accessToken: String)
+    {
+        guard userSettings != nil else
+        {
+            return
+        }
+
+        userSettings.accessToken = accessToken
+        guard OGSDiskManager.saveObject(userSettings) else
+        {
+            fatalError("Failed to save accessToken")
+        }
+    }
+
+    func save(refreshToken: String)
+    {
+        guard userSettings != nil else
+        {
+            return
+        }
+
+        userSettings.refreshToken = refreshToken
+        guard OGSDiskManager.saveObject(userSettings) else
+        {
+            fatalError("Failed to save refreshToken")
+        }
+    }
+
+    func getUserSettings() -> OGSUserSettings
+    {
+        guard userSettings == nil else
+        {
+            return userSettings
+        }
+
+        userSettings = OGSDiskManager.getData(forClass: OGSUserSettings.self)
+        if userSettings == nil
+        {
+            userSettings = OGSUserSettings()
+        }
+        return userSettings
+    }
 }
