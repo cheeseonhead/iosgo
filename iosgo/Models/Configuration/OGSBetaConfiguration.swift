9274382ec20dd4a33e9d129100c00de65e777ec5
Jeffrey Wu <jeffrey@tophat.com>
2017-02-26 00:10:07 -0500

    Added two files for configurations

---
+++ iosgo/Models/Configuration/OGSBetaConfiguration.swift
@@ -0,0 +1,11 @@
+//
+// Created by Jeffrey Wu on 2017-02-25.
+// Copyright (c) 2017 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
+
+class OGSBetaConfiguration: OGSConfigurationProtocol
+{
+
+}
---
+++ iosgo/Models/Configuration/OGSConfigurationProtocol.swift
@@ -0,0 +1,13 @@
+//
+// Created by Jeffrey Wu on 2017-02-26.
+// Copyright (c) 2017 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
+
+protocol OGSConfigurationProtocol: OGSVersionedCoding
+{
+    var domainName: String { get set }
+    var clientID: String { get set }
+    var clientSecret: String { get set }
+}
