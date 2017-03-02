0a19e4c346d7025d4bade9ef277eafafbd0d409b
Jeff Woo <cheeseonhead@gmail.com>
2017-03-01 23:27:31 -0500

    Implemented the model file

--- iosgo/Stores/SeekGraph/OGSSeekGraphModels.swift
+++ iosgo/Stores/SeekGraph/OGSSeekGraphModels.swift
@@ -4,3 +4,83 @@
 //
 
 import Foundation
+import Unbox
+
+struct OGSSeekGraphStore
+{
+    enum ChallengerColorType: String, UnboxableEnum
+    {
+        case automatic
+        case black
+        case white
+    }
+
+    enum RuleTypes: String, UnboxableEnum
+    {
+        case japanese
+        case chinese
+    }
+
+    enum SpeedTypes: String, UnboxableEnum
+    {
+        case correspondence
+    }
+
+    enum TimeControlTypes: String, UnboxableEnum
+    {
+        case fischer
+        case simple
+    }
+
+    enum TimeControlParametersType
+    {
+        case fischer(parameters: Fischer)
+        case simple(parameters: Simple)
+    }
+
+    struct Challenge
+    {
+        var username: String
+        var timePerMove: Int
+        var userId: Int
+        var name: String
+        var width: Int
+        var height: Int
+        var handicap: Int
+        var challengeId: Int
+        var pro: Int
+        var maxRank: Int
+        var minRank: Int
+        var disableAnalysis: Bool
+        var rank: Int
+        var rules: RuleTypes
+        var timeControl: TimeControlTypes
+        var ranked: Bool
+        var komi: Float?
+        var gameId: Int
+        var challengerColor: ChallengerColorType
+        var timeControlParameters: TimeControlParametersType
+    }
+
+    struct Fischer
+    {
+        var pauseOnWeekends: Bool
+        var speed: SpeedTypes
+        var system: TimeControlTypes
+        var timeControl: TimeControlTypes
+
+        var initialTime: Int
+        var maxTime: Int
+        var timeIncrement: Int
+    }
+
+    struct Simple
+    {
+        var pauseOnWeekends: Bool
+        var speed: SpeedTypes
+        var system: TimeControlTypes
+        var timeControl: TimeControlTypes
+
+        var perMove: Int
+    }
+}
