3883788a141952fb035987bd22dfdf21e53a3f29
Jeff Woo <cheeseonhead@gmail.com>
2017-03-01 23:27:42 -0500

    Removed the things from the protocol file

--- iosgo/Stores/SeekGraph/OGSSeekGraphStoreProtocol.swift
+++ iosgo/Stores/SeekGraph/OGSSeekGraphStoreProtocol.swift
@@ -4,51 +4,6 @@
 //
 
 import Foundation
-import Unbox
-
-struct OGSSeekGraphStore
-{
-    enum RuleTypes: String, UnboxableEnum
-    {
-        case japanese
-        case chinese
-    }
-
-    enum TimeControlTypes: String, UnboxableEnum
-    {
-        case fischer
-    }
-
-    enum ChallengerColorType: String, UnboxableEnum
-    {
-        case automatic
-        case black
-        case white
-    }
-
-    struct Challenge
-    {
-        var username: String
-        var timePerMove: Int
-        var userId: Int
-        var name: String
-        var width: Int
-        var height: Int
-        var handicap: Int
-        var challengeId: Int
-        var pro: Int
-        var maxRank: Int
-        var minRank: Int
-        var disableAnalysis: Bool
-        var rank: Int
-        var rules: RuleTypes
-        var timeControl: TimeControlTypes
-        var ranked: Bool
-        var komi: Float?
-        var gameId: Int
-        var challengerColor: ChallengerColorType
-    }
-}
 
 protocol OGSSeekGraphStoreDelegate: class
 {
