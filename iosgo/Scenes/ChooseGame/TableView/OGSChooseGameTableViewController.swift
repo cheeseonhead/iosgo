8ae3645d1cdbf209f811feff1b908f2887fa869d
Jeff Woo <cheeseonhead@gmail.com>
2017-03-02 00:06:43 -0500

    TableVIewController storyboard and swift file

---
+++ iosgo/Scenes/ChooseGame/TableView/OGSChooseGameTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  OGSChooseGameTableViewController.swift
+//  iosgo
+//
+//  Created by Cheese Onhead on 3/2/17.
+//  Copyright © 2017 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
