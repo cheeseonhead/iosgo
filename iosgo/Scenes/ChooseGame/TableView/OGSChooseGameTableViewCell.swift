a22b475105d533828deff2f280a684cb0387be82
Jeff Woo <cheeseonhead@gmail.com>
2017-03-02 01:22:09 -0500

    Basic outlets and setupt the button, changed colors also

--- iosgo/Scenes/ChooseGame/TableView/OGSChooseGameTableViewCell.swift
+++ iosgo/Scenes/ChooseGame/TableView/OGSChooseGameTableViewCell.swift
@@ -4,6 +4,17 @@
 //
 
 import Foundation
+import UIKit
 
-class OGSChooseGameTableViewCell {
+class OGSChooseGameTableViewCell: UITableViewCell
+{
+    @IBOutlet weak var challengerInfoLabel: UILabel!
+    @IBOutlet weak var sizeLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var playButton: OGSButton!
+    {
+        didSet {
+            playButton.setupAsButtonType(.primary)
+        }
+    }
 }
