ac980bf7936033918e7bbf44f144f34d6fd86cfe
Jeff Woo <cheeseonhead@gmail.com>
2017-03-04 23:25:28 -0500

    Completed the methods

--- iosgo/Scenes/ChooseGame/OGSChooseGameListGamesWorker.swift
+++ iosgo/Scenes/ChooseGame/OGSChooseGameListGamesWorker.swift
@@ -3,7 +3,7 @@
 // Copyright (c) 2017 Cheeseonhead. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 protocol OGSChooseGameListGamesWorkerDelegate: class
 {
@@ -23,29 +23,55 @@ class OGSChooseGameListGamesWorker: OGSSeekGraphStoreDelegate
         seekGraphStore.delegate = self
     }
 
-    typealias TimeControlParamType = ListGames.Response.Challenge.TimeControlParametersType
-
     func connect()
     {
-        let fischer = TimeControlParamType.Fischer(initialTime: 518400, maxTime: 604800, timeIncrement: 86400)
-        let param1: TimeControlParamType = .fischer(parameters: fischer)
-
-        let simple = TimeControlParamType.Simple(timePerMove: 1000000)
-        let param2: TimeControlParamType = .simple(parameters: simple)
-        let game1 = ListGames.Response.Challenge(challengerUsername: "Jeff", challengerRank: 4, minRank: 0, maxRank: 30, width: 19, height: 19, timeControlParameters: param1)
-        let game2 = ListGames.Response.Challenge(challengerUsername: "testss", challengerRank: 16, minRank: 10, maxRank: 30, width: 9, height: 9, timeControlParameters: param2)
-        let game3 = ListGames.Response.Challenge(challengerUsername: "JustRight", challengerRank: 16, minRank: 4, maxRank: 10, width: 13, height: 24, timeControlParameters: param2)
-
-        let challengeList = [game1, game2, game3, game1, game2, game3, game1, game2, game3, game1, game2, game3, game1, game2, game3]
-
-        let response = OGSChooseGame.ListGames.Response(username: "Jeff", userRank: 4, challenges: challengeList)
-        delegate?.sendListGamesResponse(response)
-
         seekGraphStore.connect()
     }
 
-    func listChallenges(_: [OGSChallenge])
+    func listChallenges(_ challenges: [OGSChallenge])
     {
+        let challengeList = createResponseChallengeList(from: challenges)
 
+        let response = OGSChooseGame.ListGames.Response(username: "Jeff", userRank: 4, challenges: challengeList)
+        delegate?.sendListGamesResponse(response)
+    }
+}
+
+fileprivate extension OGSChooseGameListGamesWorker
+{
+    func createResponseChallengeList(from challengesList: [OGSChallenge]) -> [ListGames.Response.Challenge]
+    {
+        var responseChallengeList: [ListGames.Response.Challenge] = []
+
+        for apiChallenge in challengesList {
+            let username = apiChallenge.username
+            let challengerRank = apiChallenge.challengerRank
+            let minRank = apiChallenge.minRank
+            let maxRank = apiChallenge.maxRank
+            let size = CGSize(width: apiChallenge.width, height: apiChallenge.height)
+            let timeControlParameters = createResponseTimeParameterType(from: apiChallenge.timeControlParameters)
+
+            let challenge = ListGames.Response.Challenge(challengerUsername: username, challengerRank: challengerRank,
+                    minRank: minRank, maxRank: maxRank, size: size, timeControlParameters: timeControlParameters)
+            responseChallengeList.append(challenge)
+        }
+
+        return responseChallengeList
+    }
+
+    typealias ApiTimeControlType = OGSChallenge.TimeControlParametersType
+    typealias ResponseTimControlType = ListGames.Response.Challenge.TimeControlParametersType
+
+    func createResponseTimeParameterType(from parameterType: ApiTimeControlType) -> ResponseTimControlType
+    {
+        switch parameterType {
+            case .fischer(let parameters):
+                let fischer = ResponseTimControlType.Fischer(initialTime: parameters.initialTime,
+                        maxTime: parameters.maxTime, timeIncrement: parameters.timeIncrement)
+                return .fischer(parameters: fischer)
+            case .simple(let parameters):
+                let simple = ResponseTimControlType.Simple(timePerMove: parameters.perMove)
+                return .simple(parameters: simple)
+        }
     }
 }
