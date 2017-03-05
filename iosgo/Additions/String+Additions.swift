915c48e0bbeb1171179cf2b6a1955b38964ab082
Jeff Woo <cheeseonhead@gmail.com>
2017-03-04 20:26:22 -0500

    Put the date string creation method in the extension

--- iosgo/Additions/String+Additions.swift
+++ iosgo/Additions/String+Additions.swift
@@ -5,6 +5,11 @@
 
 import Foundation
 
+fileprivate let secondsInWeek = 604800
+fileprivate let secondsInDay = 86400
+fileprivate let secondsInHour = 3600
+fileprivate let secondsInMinute = 60
+
 extension String
 {
     /// Percent escapes values to be added to a URL query as specified in RFC 3986
@@ -19,5 +24,43 @@ extension String
         let allowedCharacters = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~")
 
         return self.addingPercentEncoding(withAllowedCharacters: allowedCharacters)
+    }
+
+    static func dateStringFrom(seconds: Int) -> String
+    {
+        var dateString = ""
+        var secondsLeft = seconds
+
+        let numberOfWeeks = secondsLeft / secondsInWeek
+        if numberOfWeeks > 0 {
+            dateString.append("\(numberOfWeeks)wk ")
+        }
+        secondsLeft %= secondsInWeek
+
+        let numberOfDays = secondsLeft / secondsInDay
+        if numberOfDays > 0 {
+            dateString.append("\(numberOfDays)d ")
+        }
+        secondsLeft %= secondsInDay
+
+        let numberOfHours = secondsLeft / secondsInHour
+        if numberOfHours > 0 {
+            dateString.append("\(numberOfHours)hr ")
+        }
+        secondsLeft %= secondsInHour
+
+        let numberOfMinutes = secondsLeft / secondsInMinute
+        if numberOfMinutes > 0 {
+            dateString.append("\(numberOfMinutes)hr ")
+        }
+        secondsLeft %= secondsInMinute
+
+        if secondsLeft > 0 {
+            dateString.append("\(secondsLeft)s")
+        }
+
+        dateString = dateString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+
+        return dateString
     }
 }
