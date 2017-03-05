7876f26eb5b9e977cde4244f55c7ea2bf4e9d9c1
Jeff Woo <cheeseonhead@gmail.com>
2017-03-05 04:21:46 -0500

    Added a method to string extension

--- iosgo/Additions/String+Additions.swift
+++ iosgo/Additions/String+Additions.swift
@@ -63,4 +63,15 @@ extension String
 
         return dateString
     }
+
+    func convertToDictionary() -> [String: Any]? {
+        if let data = self.data(using: .utf8) {
+            do {
+                return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
+    }
 }
