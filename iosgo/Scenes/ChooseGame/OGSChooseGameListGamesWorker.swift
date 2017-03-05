0916c95b3a7a5ff8dc6a62591e360e694b69d4a7
Jeff Woo <cheeseonhead@gmail.com>
2017-03-04 21:33:41 -0500

    Dummy data in the worker

--- iosgo/Scenes/ChooseGame/OGSChooseGameListGamesWorker.swift
+++ iosgo/Scenes/ChooseGame/OGSChooseGameListGamesWorker.swift
@@ -12,6 +12,8 @@ protocol OGSChooseGameListGamesWorkerDelegate: class
 
 class OGSChooseGameListGamesWorker: OGSSeekGraphStoreDelegate
 {
+    typealias ListGames = OGSChooseGame.ListGames
+
     weak var delegate: OGSChooseGameListGamesWorkerDelegate?
     var seekGraphStore: OGSSeekGraphStoreProtocol!
 
@@ -21,8 +23,24 @@ class OGSChooseGameListGamesWorker: OGSSeekGraphStoreDelegate
         seekGraphStore.delegate = self
     }
 
+    typealias TimeControlParamType = ListGames.Response.Challenge.TimeControlParametersType
+
     func connect()
     {
+        let fischer = TimeControlParamType.Fischer(initialTime: 518400, maxTime: 604800, timeIncrement: 86400)
+        let param1: TimeControlParamType = .fischer(parameters: fischer)
+
+        let simple = TimeControlParamType.Simple(timePerMove: 1000000)
+        let param2: TimeControlParamType = .simple(parameters: simple)
+        let game1 = ListGames.Response.Challenge(challengerUsername: "Jeff", challengerRank: 4, minRank: 0, maxRank: 30, width: 19, height: 19, timeControlParameters: param1)
+        let game2 = ListGames.Response.Challenge(challengerUsername: "testss", challengerRank: 16, minRank: 10, maxRank: 30, width: 9, height: 9, timeControlParameters: param2)
+        let game3 = ListGames.Response.Challenge(challengerUsername: "JustRight", challengerRank: 16, minRank: 4, maxRank: 10, width: 13, height: 24, timeControlParameters: param2)
+
+        let challengeList = [game1, game2, game3, game1, game2, game3, game1, game2, game3, game1, game2, game3, game1, game2, game3]
+
+        let response = OGSChooseGame.ListGames.Response(username: "Jeff", userRank: 4, challenges: challengeList)
+        delegate?.sendListGamesResponse(response)
+
         seekGraphStore.connect()
     }
 
