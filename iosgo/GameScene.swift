86372939c3be26c3e4d92739c58e035082160f92
Jeff Woo <cheeseonhead@gmail.com>
2017-03-31 02:39:10 -0400

    Warning

--- iosgo/GameScene.swift
+++ iosgo/GameScene.swift
@@ -30,7 +30,7 @@ class GameScene: SKScene {
         if let spinnyNode = self.spinnyNode {
             spinnyNode.lineWidth = 2.5
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
+            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
             spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
                                               SKAction.fadeOut(withDuration: 0.5),
                                               SKAction.removeFromParent()]))
