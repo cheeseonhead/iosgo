18225ff46c36c99b940347db27c0bb031b1d5a8d
Jeffrey Wu <jeffrey@tophat.com>
2017-04-10 23:39:38 -0400

    Added the cell view

---
+++ iosgo/Scenes/ChooseGame/CollectionView/OGSChooseGameCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  OGSChooseGameCollectionViewCell.swift
+//  iosgo
+//
+//  Created by Jeffrey Wu on 2017-04-10.
+//  Copyright © 2017 Cheeseonhead. All rights reserved.
+//
+
+import UIKit
+
+class OGSChooseGameCollectionViewCell: UICollectionViewCell
+{
+
+}
