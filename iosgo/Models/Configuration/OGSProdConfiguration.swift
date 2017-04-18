1b22d279135e794fb06850587ad1694601b719dd
Jeff Woo <cheeseonhead@gmail.com>
2017-04-18 00:38:49 -0400

    Added Prod config

---
+++ iosgo/Models/Configuration/OGSProdConfiguration.swift
@@ -0,0 +1,13 @@
+//
+// Created by Cheese Onhead on 4/18/17.
+// Copyright (c) 2017 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
+
+class OGSProdConfiguration: OGSConfigurationProtocol
+{
+    var domainName = "https://online-go.com/"
+    var clientID = "IR7GqvOdPAWD4C70BXJrLKt0IG5cy7IacCL7wo9G"
+    var clientSecret = "H0hmz28CiVRitYcsbCdnWa33WC3OstnmVSyJdTy2sf1TWoSQ16cOcWH6zvILKyiUFHqgcEazjUlnv9nFberfHHVSiH6HpBgxzmeBjDwNV1gkeVP8jtaLh7GNXASoLXG6"
+}
