567b9d6f1872869cee3b9d68e1834cda4779e716
Jeff Woo <cheeseonhead@gmail.com>
2017-04-21 00:45:45 -0400

    Started having a blue print for the stuff

--- iosgo/Stores/SeekGraph/OGSSeekGraphSocketStore.swift
+++ iosgo/Stores/SeekGraph/OGSSeekGraphSocketStore.swift
@@ -11,9 +11,9 @@ class OGSSeekGraphSocketStore
 {
     enum ModelType
     {
-        case challenge
-        case gameStarted
-        case deleteChallenge
+        case challengeList([OGSSeekGraphSocketStoreModel.Challenge])
+        case gameStarted(OGSSeekGraphSocketStoreModel.GameStart)
+        case deleteChallenge(OGSSeekGraphSocketStoreModel.ChallengeDelete)
     }
 
     weak var delegate: OGSListGamesStoreDelegate?
@@ -21,15 +21,20 @@ class OGSSeekGraphSocketStore
 
     func connect()
     {
-        let challenge = createChallengeFrom(payload: fakeData1())
-        delegate?.listChallenges([challenge])
+        //        let challenge = createChallengeFrom(payload: fakeData1())
+        //        delegate?.listChallenges([challenge])
 
         socketManager.on(event: .seekGraphGlobal)
-        { _ in
-            print("Hello")
+        { array in
+            self.modelType(data: array)
         }
 
         socketManager.emit(event: .seekGraphConnect, with: ["channel": "global"])
+    }
+
+    func modelType(data: [Any]) -> ModelType
+    {
+        return .challengeList([])
     }
 
     func createChallengeFrom(payload: [String: Any]) -> OGSChallenge
@@ -43,39 +48,5 @@ class OGSSeekGraphSocketStore
         {
             fatalError("Unable to parse Challenge")
         }
-    }
-
-    func fakeData1() -> [String: Any]
-    {
-        return [
-            "username": "sousys",
-            "time_per_move": 89280,
-            "user_id": 157,
-            "name": "Friendly Match",
-            "width": 19,
-            "handicap": -1,
-            "challenge_id": 767,
-            "pro": 0,
-            "max_rank": 3,
-            "disable_analysis": false,
-            "rank": 0,
-            "height": 19,
-            "rules": "japanese",
-            "time_control": "fischer",
-            "ranked": true,
-            "min_rank": -3,
-            //            "komi": nil,
-            "game_id": 809,
-            "challenger_color": "automatic",
-            "time_control_parameters": [
-                "system": "fischer",
-                "pause_on_weekends": true,
-                "time_control": "fischer",
-                "initial_time": 259_200,
-                "max_time": 604_800,
-                "time_increment": 86400,
-                "speed": "correspondence",
-            ],
-        ]
     }
 }
