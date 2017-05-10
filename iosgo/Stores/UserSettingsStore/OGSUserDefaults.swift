cb3a5f7a58b9a83c303d68bd85ec74b1dbfd880d
Jeffrey Wu <jeffrey@tophat.com>
2017-05-10 11:34:44 -0400

    Added a new user settings store

---
+++ iosgo/Stores/UserSettingsStore/OGSUserDefaults.swift
@@ -0,0 +1,21 @@
+//
+// Created by Jeffrey Wu on 2017-05-10.
+// Copyright (c) 2017 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
+
+class OGSUserDefaults: OGSUserSettingsStoreProtocol
+{
+    func save(accessToken _: String)
+    {
+    }
+
+    func save(refreshToken _: String)
+    {
+    }
+
+    func getUserSettings() -> OGSUserSettings
+    {
+    }
+}
