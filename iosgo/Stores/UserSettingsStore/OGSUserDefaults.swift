e667b3f459cd043494d87e593c38a5cf9484b76d
Jeffrey Wu <jeffrey@tophat.com>
2017-05-10 14:55:26 -0400

    Switched over to userDefaults to store persistent information

--- iosgo/Stores/UserSettingsStore/OGSUserDefaults.swift
+++ iosgo/Stores/UserSettingsStore/OGSUserDefaults.swift
@@ -7,15 +7,30 @@ import Foundation
 
 class OGSUserDefaults: OGSUserSettingsStoreProtocol
 {
-    func save(accessToken _: String)
+    enum Key
     {
+        static let accessToken = "accessToken"
+        static let refreshToken = "refreshToken"
     }
 
-    func save(refreshToken _: String)
+    var userDefault: UserDefaults = UserDefaults.standard
+
+    func save(accessToken: String)
     {
+        userDefault.set(accessToken, forKey: Key.accessToken)
+    }
+
+    func save(refreshToken: String)
+    {
+        userDefault.set(refreshToken, forKey: Key.refreshToken)
     }
 
     func getUserSettings() -> OGSUserSettings
     {
+        let settings = OGSUserSettings()
+        settings.accessToken = userDefault.string(forKey: Key.accessToken)
+        settings.refreshToken = userDefault.string(forKey: Key.refreshToken)
+
+        return settings
     }
 }
