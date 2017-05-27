9cfd0b6c7252641a7fa7f54fcdc7b5390d3a70d2
Jeff Woo <cheeseonhead@gmail.com>
2017-05-27 16:28:32 -0400

    #9 | Added this user class

---
+++ iosgo/Models/OGSUser.swift
@@ -0,0 +1,12 @@
+//
+// Created by Cheese Onhead on 5/27/17.
+// Copyright (c) 2017 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
+
+class OGSUser
+{
+    var username: String
+    var rank: Int
+}
