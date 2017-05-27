4d2d447dd71f9f55460a477822d4182758764a3e
Jeff Woo <cheeseonhead@gmail.com>
2017-05-27 16:17:55 -0400

    #9 | Added SessionWorker

---
+++ iosgo/Workers/OGSSessionWorker.swift
@@ -0,0 +1,16 @@
+//
+// Created by Cheese Onhead on 5/27/17.
+// Copyright (c) 2017 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
+
+class OGSSessionWorker
+{
+    var sessionController: OGSSessionController
+
+    required init(sessionController: OGSSessionController)
+    {
+        self.sessionController = sessionController
+    }
+}
