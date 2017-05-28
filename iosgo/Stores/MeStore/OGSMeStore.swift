bd865536beacea5375bb28dfeb09ca4b990b3a65
Jeff Woo <cheeseonhead@gmail.com>
2017-05-27 21:22:29 -0400

    #9 | Added some code in the function

--- iosgo/Stores/MeStore/OGSMeStore.swift
+++ iosgo/Stores/MeStore/OGSMeStore.swift
@@ -7,12 +7,50 @@ import Foundation
 
 class OGSMeStore
 {
-    static let url = "api/v1/me/"
+    struct Response
+    {
+        var result: Result
 
+        enum Result
+        {
+            case success
+            case error(type: ErrorType)
+        }
+
+        enum ErrorType
+        {
+            case unauthorized
+            case clientError
+            case unknownError
+        }
+    }
+
+    let url = "api/v1/me/"
     var apiStore: OGSApiStore
 
     required init(apiStore: OGSApiStore)
     {
         self.apiStore = apiStore
+    }
+
+    func getUser(completion: @escaping (Response) -> Void)
+    {
+        apiStore.request(toUrl: url, method: .GET, parameters: [:])
+        { statusCode, _, _ in
+            var response = Response(result: .error(type: .unknownError))
+
+            switch statusCode {
+            case .ok:
+                response.result = .success
+            case .unauthorized:
+                response.result = .error(type: .unauthorized)
+            case .clientError:
+                response.result = .error(type: .clientError)
+            default:
+                break
+            }
+
+            completion(response)
+        }
     }
 }
