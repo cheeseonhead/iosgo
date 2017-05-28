9c2e0a5456ad306803369860c5d2e71b27d3df0d
Jeff Woo <cheeseonhead@gmail.com>
2017-05-27 23:51:47 -0400

    #9 | Get the user when a new session is assigned

--- iosgo/Managers/OGSSessionController.swift
+++ iosgo/Managers/OGSSessionController.swift
@@ -10,6 +10,16 @@ class OGSSessionController
     static let sharedInstance = OGSSessionController(session: OGSSession(configuration: OGSMockConfiguration()))
 
     var current: OGSSession
+    {
+        didSet
+        {
+            if current.user == nil
+            {
+                let meStore = OGSMeStore(apiStore: OGSApiStore(sessionController: self), sessionController: self)
+                meStore.getUser { _ in }
+            }
+        }
+    }
 
     required init(session: OGSSession)
     {
