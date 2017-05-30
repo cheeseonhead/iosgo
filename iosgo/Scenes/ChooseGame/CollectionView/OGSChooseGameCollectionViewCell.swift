0b0281dc6be92081bc7cebc1da915aaf494b8038
Jeff Woo <cheeseonhead@gmail.com>
2017-05-30 03:32:02 -0400

    #7 | Correctly use the button in the cell

--- iosgo/Scenes/ChooseGame/CollectionView/OGSChooseGameCollectionViewCell.swift
+++ iosgo/Scenes/ChooseGame/CollectionView/OGSChooseGameCollectionViewCell.swift
@@ -45,17 +45,14 @@ class OGSChooseGameCollectionViewCell: UICollectionViewCell
     {
         switch type {
         case .play:
-            button.backgroundColor = #colorLiteral(red: 0.4431372549, green: 0.5254901961, blue: 0.7803921569, alpha: 1)
             button.setTitle(NSLocalizedString("Play", comment: ""), for: UIControlState.normal)
-            button.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: UIControlState.normal)
+            button.customState = .normal
         case .cantPlay:
-            button.backgroundColor = #colorLiteral(red: 0.9450980392, green: 0.9568627451, blue: 0.9882352941, alpha: 1)
-            button.setTitle(NSLocalizedString("Can't Accept", comment: ""), for: UIControlState.normal)
-            button.setTitleColor(#colorLiteral(red: 0.6666666667, green: 0.7098039216, blue: 0.8274509804, alpha: 1), for: UIControlState.normal)
+            button.setTitle(NSLocalizedString("Can't Accept", comment: ""), for: UIControlState.disabled)
+            button.customState = .disabled
         case .remove:
-            button.backgroundColor = #colorLiteral(red: 0.4431372549, green: 0.5254901961, blue: 0.7803921569, alpha: 1)
             button.setTitle(NSLocalizedString("Remove", comment: ""), for: UIControlState.normal)
-            button.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: UIControlState.normal)
+            button.customState = .normal
         }
     }
 }
