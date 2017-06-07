f45cc79af3ddb9c2ce5bf4fa3b64d54a28afb44f
Jeff Woo <cheeseonhead@gmail.com>
2017-06-07 00:17:15 -0400

    #20 | Added two more codes and privated some methods

--- iosgo/Stores/OGSApiStore.swift
+++ iosgo/Stores/OGSApiStore.swift
@@ -5,24 +5,23 @@
 
 import Foundation
 
-enum HTTPMethod: String
-{
+enum HTTPMethod: String {
     case GET
     case POST
 }
 
-enum HTTPStatusCode: Int
-{
+enum HTTPStatusCode: Int {
     case clientError = -1
     case ok = 200
+    case accepted = 202
     case badRequest = 400
     case unauthorized = 401
+    case forbidden = 403
     case notFound = 404
     case tooManyRequests = 429
 }
 
-enum ApiErrorType
-{
+enum ApiErrorType {
     case unauthorized
     case unknownError
     case clientError
@@ -30,54 +29,42 @@ enum ApiErrorType
 
 typealias OGSApiResultBlock = (_ statusCode: HTTPStatusCode, _ payload: [String: Any]?, _ error: Error?) -> Void
 
-class OGSApiStore
-{
+class OGSApiStore {
     var sessionController: OGSSessionController
-    var session: OGSSession
-    {
+    var session: OGSSession {
         return sessionController.current
     }
-    var clientID: String
-    {
+    var clientID: String {
         return session.configuration.clientID
     }
     var clientSecret: String! {
         return session.configuration.clientSecret
     }
-    var domainName: String
-    {
+    var domainName: String {
         return session.configuration.domainName
     }
-    var accessToken: String?
-    {
-        get
-        {
+    var accessToken: String? {
+        get {
             return session.accessToken
         }
-        set
-        {
+        set {
             sessionController.current.accessToken = newValue
         }
     }
-    var refreshToken: String?
-    {
-        get
-        {
+    var refreshToken: String? {
+        get {
             return session.refreshToken
         }
-        set
-        {
+        set {
             sessionController.current.refreshToken = newValue
         }
     }
 
-    required init(sessionController: OGSSessionController)
-    {
+    required init(sessionController: OGSSessionController) {
         self.sessionController = sessionController
     }
 
-    func request(toUrl url: String, method: HTTPMethod, parameters: [String: String], completion: @escaping OGSApiResultBlock)
-    {
+    func request(toUrl url: String, method: HTTPMethod, parameters: [String: String], completion: @escaping OGSApiResultBlock) {
         guard let fullURL = URL(string: domainName.appending(url)) else { return }
 
         let request = createRequest(fullURL: fullURL, method: method, parameters: parameters)
@@ -85,13 +72,11 @@ class OGSApiStore
         send(request: request, completion: completion)
     }
 
-    func createRequest(fullURL: URL, method: HTTPMethod, parameters: [String: String]) -> URLRequest
-    {
+    private func createRequest(fullURL: URL, method: HTTPMethod, parameters: [String: String]) -> URLRequest {
         var request = URLRequest(url: fullURL)
         request.httpMethod = method.rawValue
         request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        if let token = accessToken
-        {
+        if let token = accessToken {
             request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
         }
         request.httpBody = parameters.stringFromHttpParameters().data(using: .utf8)
@@ -99,23 +84,17 @@ class OGSApiStore
         return request
     }
 
-    func send(request: URLRequest, completion: @escaping OGSApiResultBlock)
-    {
-        let task = URLSession.shared.dataTask(with: request)
-        { data, response, error in
-            guard let httpResponse = response as? HTTPURLResponse, let data = data else
-            {
+    private func send(request: URLRequest, completion: @escaping OGSApiResultBlock) {
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let httpResponse = response as? HTTPURLResponse, let data = data else {
                 completion(.clientError, nil, error)
                 return
             }
 
-            do
-            {
+            do {
                 let json = try JSONSerialization.jsonObject(with: data) as? [String: Any]
                 completion(HTTPStatusCode(rawValue: httpResponse.statusCode)!, json, error)
-            }
-            catch _
-            {
+            } catch _ {
                 print("Error Occurred")
             }
         }
