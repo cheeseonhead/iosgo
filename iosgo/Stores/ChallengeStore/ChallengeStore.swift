8abc7e4c951b75d0f1c6329752d0ec1ed5641636
Jeff Woo <cheeseonhead@gmail.com>
2017-06-07 00:17:48 -0400

    #20 | Implemented the challengeStore

--- iosgo/Stores/ChallengeStore/ChallengeStore.swift
+++ iosgo/Stores/ChallengeStore/ChallengeStore.swift
@@ -3,21 +3,38 @@
 // Copyright (c) 2017 Cheeseonhead. All rights reserved.
 //
 
-class ChallengeStore
-{
-    struct AcceptResponse
-    {
+class ChallengeStore {
+    struct AcceptResponse {
         var success: Bool
     }
 
     fileprivate var apiStore: OGSApiStore
 
-    init(apiStore: OGSApiStore)
-    {
+    init(apiStore: OGSApiStore) {
         self.apiStore = apiStore
     }
 
-    func acceptChallenge(id _: Int, completion _: (_: AcceptResponse) -> Void)
-    {
+    func acceptChallenge(id: Int, completion: @escaping (_: AcceptResponse) -> Void) {
+        let url = ChallengeURLGenerator.accept(challengeId: id)
+
+        apiStore.request(toUrl: url, method: .POST, parameters: [:]) { statusCode, _, _ in
+            var response = AcceptResponse(success: false)
+            switch statusCode {
+            case .accepted:
+                response.success = true
+            default:
+                break
+            }
+
+            completion(response)
+        }
+    }
+}
+
+private class ChallengeURLGenerator {
+    private static let baseURL = "api/v1/challenges/"
+
+    class func accept(challengeId: Int) -> String {
+        return "\(baseURL)\(challengeId)/accept"
     }
 }
