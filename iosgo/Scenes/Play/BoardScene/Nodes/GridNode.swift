c0e44c1aeb0b67021dfec60b834733018f801e8f
Jeff Woo <cheeseonhead@gmail.com>
2017-06-10 03:17:43 -0400

    #30 | Added grid node file

---
+++ iosgo/Scenes/Play/BoardScene/Nodes/GridNode.swift
@@ -0,0 +1,12 @@
+//
+//  GridNode.swift
+//  iosgo
+//
+//  Created by Cheese Onhead on 6/10/17.
+//  Copyright © 2017 Cheeseonhead. All rights reserved.
+//
+
+import SpriteKit
+
+class GridNode: SKSpriteNode {
+}
