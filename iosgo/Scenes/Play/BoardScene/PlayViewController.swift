35912a39d924750837ab474abcdcf27f0505a1d1
Jeff Woo <cheeseonhead@gmail.com>
2017-06-10 04:04:38 -0400

    #30 | Added some things

---
+++ iosgo/Scenes/Play/BoardScene/PlayViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PlayViewController.swift
+//  iosgo
+//
+//  Created by Cheese Onhead on 6/10/17.
+//  Copyright © 2017 Cheeseonhead. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class PlayViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "BoardScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFit
+
+                // Present the scene
+                view.presentScene(scene)
+            }
+
+            view.ignoresSiblingOrder = true
+
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
