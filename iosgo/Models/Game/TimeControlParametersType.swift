e24803e72efaedacc55687a965e93dbd8db56f20
Jeff Woo <cheeseonhead@gmail.com>
2017-06-24 23:27:33 -0400

    #34 | Moved the ogsChallenge to somewhere else

--- iosgo/Models/Game/TimeControlParametersType.swift
+++ iosgo/Models/Game/TimeControlParametersType.swift
@@ -10,96 +10,69 @@ import Foundation
 import Unbox
 
 enum TimeControlParametersType {
-    
+
     case fischer(parameters: Fischer)
     case simple(parameters: Simple)
     case byoyomi(parameters: Byoyomi)
     case canadian(parameters: Canadian)
     case absolute(parameters: Absolute)
     case none(parameters: None)
-    
+
     struct Fischer: Codable {
         var pauseOnWeekends: Bool
         var speed: SpeedTypes
         var system: TimeControlTypes
         var timeControl: TimeControlTypes
-        
+
         var initialTime: Int
         var maxTime: Int
         var timeIncrement: Int
     }
-    
+
     struct Simple: Codable {
         var pauseOnWeekends: Bool
         var speed: SpeedTypes
         var system: TimeControlTypes
         var timeControl: TimeControlTypes
-        
+
         var timePerMove: Int
     }
-    
+
     struct Byoyomi: Codable {
         var pauseOnWeekends: Bool
         var speed: SpeedTypes
         var system: TimeControlTypes
         var timeControl: TimeControlTypes
-        
+
         var mainTime: Int
         var periodTime: Int
         var periodCount: Int
     }
-    
+
     struct Canadian: Codable {
         var pauseOnWeekends: Bool
         var speed: SpeedTypes
         var system: TimeControlTypes
         var timeControl: TimeControlTypes
-        
+
         var mainTime: Int
         var periodTime: Int
         var stonesPerPeriod: Int
     }
-    
+
     struct Absolute: Codable {
         var pauseOnWeekends: Bool
         var speed: SpeedTypes
         var system: TimeControlTypes
         var timeControl: TimeControlTypes
-        
+
         var totalTime: Int
     }
-    
+
     struct None: Codable {
         var pauseOnWeekends: Bool
         var speed: SpeedTypes
         var system: TimeControlTypes
         var timeControl: TimeControlTypes
-    }
-    
-    static func from(timeControl: TimeControlTypes, unboxable: UnboxableDictionary) throws -> TimeControlParametersType {
-        
-        let wrapperDict: UnboxableDictionary = ["time_control_parameters": unboxable]
-        let unboxer = Unboxer(dictionary: wrapperDict)
-        
-        switch timeControl {
-        case .fischer:
-            let parameters: TimeControlParametersType.Fischer = try unboxer.unbox(key: "time_control_parameters")
-            return .fischer(parameters: parameters)
-        case .simple:
-            let parameters: TimeControlParametersType.Simple = try unboxer.unbox(key: "time_control_parameters")
-            return .simple(parameters: parameters)
-        case .byoyomi:
-            let parameters: TimeControlParametersType.Byoyomi = try unboxer.unbox(key: "time_control_parameters")
-            return .byoyomi(parameters: parameters)
-        case .canadian:
-            let parameters: TimeControlParametersType.Canadian = try unboxer.unbox(key: "time_control_parameters")
-            return .canadian(parameters: parameters)
-        case .absolute:
-            let parameters: TimeControlParametersType.Absolute = try unboxer.unbox(key: "time_control_parameters")
-            return .absolute(parameters: parameters)
-        case .none:
-            let parameters: TimeControlParametersType.None = try unboxer.unbox(key: "time_control_parameters")
-            return .none(parameters: parameters)
-        }
     }
 }
---
+++ iosgo/Stores/Models/OGSChallenge+Unboxable.swift
@@ -0,0 +1,37 @@
+//
+//  OGSChallenge+Unboxable.swift
+//  iosgo
+//
+//  Created by Cheese Onhead on 6/24/17.
+//  Copyright © 2017 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
+import Unbox
+
+extension OGSChallenge: Unboxable {
+    init(unboxer: Unboxer) throws {
+        username = try unboxer.unbox(key: "username")
+        name = try unboxer.unbox(key: "name")
+        timePerMove = try unboxer.unbox(key: "time_per_move")
+        userId = try unboxer.unbox(key: "user_id")
+        width = try unboxer.unbox(key: "width")
+        height = try unboxer.unbox(key: "height")
+        handicap = try unboxer.unbox(key: "handicap")
+        id = try unboxer.unbox(key: "challenge_id")
+        pro = try unboxer.unbox(key: "pro")
+        maxRank = try unboxer.unbox(key: "max_rank")
+        disableAnalysis = try unboxer.unbox(key: "disable_analysis")
+        challengerRank = try unboxer.unbox(key: "rank")
+        rules = try unboxer.unbox(key: "rules")
+        timeControl = try unboxer.unbox(key: "time_control")
+        ranked = try unboxer.unbox(key: "ranked")
+        minRank = try unboxer.unbox(key: "min_rank")
+        komi = unboxer.unbox(key: "komi")
+        gameId = try unboxer.unbox(key: "game_id")
+        challengerColor = try unboxer.unbox(key: "challenger_color")
+
+        let formatter = TimeControlParametersFormatter()
+        timeControlParameters = try unboxer.unbox(key: "time_control_parameters", formatter: formatter)
+    }
+}
