e6f24851dffb2879c06fd564de6b7ff77de7e616
Jeff Woo <cheeseonhead@gmail.com>
2017-06-26 21:27:12 -0400

    #40 | Added two files

---
+++ iosgo/Engine/BoardPoint.swift
@@ -0,0 +1,15 @@
+//
+//  BoardPoint.swift
+//  iosgo
+//
+//  Created by Cheese Onhead on 6/26/17.
+//  Copyright © 2017 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
+
+// Starts with (0, 0) at the top left of the board
+struct BoardPoint {
+    var row: Int
+    var column: Int
+}
---
+++ iosgo/Engine/GameEngine.swift
@@ -0,0 +1,14 @@
+//
+//  GameEngine.swift
+//  iosgo
+//
+//  Created by Cheese Onhead on 6/26/17.
+//  Copyright © 2017 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
+
+class GameEngine {
+
+    func place(at)
+}
