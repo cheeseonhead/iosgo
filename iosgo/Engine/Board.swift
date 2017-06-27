9fedb6a4777064d79b206916adaa83de98e46e9e
Jeff Woo <cheeseonhead@gmail.com>
2017-06-26 21:56:16 -0400

    #40 | Started imple of board

--- iosgo/Engine/Board.swift
+++ iosgo/Engine/Board.swift
@@ -10,7 +10,13 @@ import Foundation
 
 struct Board {
 
+    var size: BoardSize
+
     private var board = [BoardPoint: Stone]()
+
+    init(size: BoardSize) {
+        self.size = size
+    }
 
     mutating func place(stone: Stone, at point: BoardPoint) {
         board[point] = stone
