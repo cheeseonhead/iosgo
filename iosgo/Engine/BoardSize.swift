c60e59b189fd6df6da5b179812a6cdb047ea063d
Jeff Woo <cheeseonhead@gmail.com>
2017-06-26 21:58:24 -0400

    #40 | One helper method

--- iosgo/Engine/BoardSize.swift
+++ iosgo/Engine/BoardSize.swift
@@ -11,4 +11,16 @@ import Foundation
 struct BoardSize {
     var rows: Int
     var columns: Int
+
+    func contains(point: BoardPoint) -> Bool {
+        guard point.row >= 0 && point.column >= 0 else {
+            return false
+        }
+
+        guard point.row < rows && point.column < columns else {
+            return false
+        }
+
+        return true
+    }
 }
