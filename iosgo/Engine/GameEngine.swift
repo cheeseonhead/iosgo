5b3a199c9f30c9ac923e7d547d14d43a1c5587b5
Jeff Woo <cheeseonhead@gmail.com>
2017-06-26 23:38:39 -0400

    #40 | Implemented three methods

--- iosgo/Engine/GameEngine.swift
+++ iosgo/Engine/GameEngine.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+private typealias Group = [BoardPoint]
+
 class GameEngine {
 
     var currentMarker = 0
@@ -19,6 +21,14 @@ class GameEngine {
     required init(game: Game) {
         playingPlayer = (game.gameData.initialPlayer == .black) ? .black : .white
         self.board = Board(size: BoardSize(rows: game.height, columns: game.width))
+
+        for move in game.gameData.moves {
+            do {
+                try place(at: move, checkForKo: false, errorOnSuperKo: false, dontCheckForSuperKo: true, dontCheckForSuicide: true, isTrunkMove: true)
+            } catch {
+                print("Error occurred while placing: \(error)")
+            }
+        }
     }
 
     func place(at point: BoardPoint, checkForKo _: Bool = false, errorOnSuperKo _: Bool = false, dontCheckForSuperKo _: Bool = false, dontCheckForSuicide _: Bool = false, isTrunkMove _: Bool = false) throws {
@@ -26,7 +36,7 @@ class GameEngine {
             return
         }
 
-        guard board.stone(at: point) != nil else {
+        guard board.stone(at: point) == nil else {
             return
         }
 
@@ -34,16 +44,20 @@ class GameEngine {
 
         let suicideMove = false
         let playerGroup = getGroup(at: point, clearMarks: true)
+        let opponentGroups = getConnectedGroups(to: playerGroup)
 
         print(suicideMove)
-        print(playerGroup)
+        print("PlayerGroup: \(playerGroup)")
+        print("OpponentGroup: \(opponentGroups)")
+
+        playingPlayer = (playingPlayer == .black) ? .white : .black
     }
 }
 
 // MARK: - Groups
-extension GameEngine {
+private extension GameEngine {
 
-    func getGroup(at point: BoardPoint, clearMarks: Bool) -> [BoardPoint] {
+    func getGroup(at point: BoardPoint, clearMarks: Bool) -> Group {
         let targetStoneType = board.stone(at: point)?.type
 
         if clearMarks {
@@ -73,6 +87,51 @@ extension GameEngine {
         return ret
     }
 
+    func getConnectedGroups(to group: Group) -> [Group] {
+        currentMarker += 1
+        markGroup(group)
+
+        var ret = [Group]()
+        forEachNeighbor(of: group) { neighbor in
+            guard board.stone(at: neighbor) != nil else {
+                return
+            }
+
+            currentMarker += 1
+            markGroup(group)
+
+            for addedGroup in ret {
+                markGroup(addedGroup)
+            }
+
+            let nextGroup = getGroup(at: neighbor, clearMarks: false)
+            if nextGroup.count > 0 {
+                ret.append(nextGroup)
+            }
+        }
+
+        return ret
+    }
+
+    func markGroup(_ group: Group) {
+        for point in group {
+            markGrid[point] = currentMarker
+        }
+    }
+}
+
+// MARK: - Helpers
+private extension GameEngine {
+
+    func insertStone(for player: PlayerType, at point: BoardPoint) {
+        switch player {
+        case .black:
+            board.placeStone(type: .black, at: point)
+        case .white:
+            board.placeStone(type: .white, at: point)
+        }
+    }
+
     func forEachNeighbor(of point: BoardPoint, reducer: (BoardPoint) -> Void) {
         let pointsToCheck = [point.point(at: .above), point.point(at: .below), point.point(at: .left), point.point(at: .right)]
         for checkPoint in pointsToCheck {
@@ -81,17 +140,22 @@ extension GameEngine {
             }
         }
     }
-}
 
-// MARK: - Helpers
-extension GameEngine {
+    func forEachNeighbor(of group: Group, reducer: (BoardPoint) -> Void) {
+        var visitedPoints = Set<BoardPoint>()
+        for point in group {
+            visitedPoints.insert(point)
+        }
 
-    func insertStone(for player: PlayerType, at point: BoardPoint) {
-        switch player {
-        case .black:
-            board.placeStone(type: .black, at: point)
-        case .white:
-            board.placeStone(type: .white, at: point)
+        for checkPoint in group {
+            forEachNeighbor(of: checkPoint, reducer: { neighbor in
+                guard !visitedPoints.contains(neighbor) else {
+                    return
+                }
+
+                visitedPoints.insert(neighbor)
+                reducer(neighbor)
+            })
         }
     }
 }
