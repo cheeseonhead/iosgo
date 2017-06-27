ed7a6eac9fa073894a9a61bee4348aa632625d3a
Jeff Woo <cheeseonhead@gmail.com>
2017-06-26 22:49:20 -0400

    #40 | needed a new stone type for the engine

---
+++ iosgo/Engine/BoardStone.swift
@@ -0,0 +1,13 @@
+//
+//  BoardStone.swift
+//  iosgo
+//
+//  Created by Cheese Onhead on 6/26/17.
+//  Copyright © 2017 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
+
+struct BoardStone {
+    var type: StoneType
+}
