2b0d69145c5defde01809f0ee0a993939f3080ba
Jeff Woo <cheeseonhead@gmail.com>
2017-07-02 16:52:36 -0400

    #47 | TWo methods for stoneDiffWorker

--- iosgo/Scenes/Play/BoardScene/Workers/StoneDiffWorker.swift
+++ iosgo/Scenes/Play/BoardScene/Workers/StoneDiffWorker.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+
+class StoneDiffWorker {
+
+    private var stoneWorker: StoneWorker
+    private var newState: GridState
+
+    required init(stoneWorker: StoneWorker, newState: GridState) {
+        self.stoneWorker = stoneWorker
+        self.newState = newState
+    }
+
+    func stonesToPlace() -> [GridStone] {
+
+        var stones = [GridStone]()
+
+        for row in 1 ... newState.size.height {
+            for col in 1 ... newState.size.width {
+
+                let curPoint = GridPoint(row: row, col: col)
+                if newState.stones[curPoint] != stoneWorker.stone(at: curPoint),
+                    let stateStone = newState.stones[curPoint] {
+                    stones.append(stateStone)
+                }
+            }
+        }
+
+        return stones
+    }
+
+    func stonesToRemove() -> [GridPoint] {
+
+        var points = [GridPoint]()
+
+        for row in 1 ... newState.size.height {
+            for col in 1 ... newState.size.width {
+
+                let curPoint = GridPoint(row: row, col: col)
+                if newState.stones[curPoint] == nil, stoneWorker.isOccupied(point: curPoint) {
+                    points.append(curPoint)
+                }
+            }
+        }
+
+        return points
+    }
+}
