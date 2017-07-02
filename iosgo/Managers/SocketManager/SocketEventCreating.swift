3587b2571b975e83071fd4fe1bf66e4fc8922dd4
Jeff Woo <cheeseonhead@gmail.com>
2017-07-02 04:16:20 -0400

    #47 | New thing

---
+++ iosgo/Managers/SocketManager/SocketEventCreating.swift
@@ -0,0 +1,13 @@
+//
+//  SocketEventCreator.swift
+//  iosgo
+//
+//  Created by Cheese Onhead on 7/2/17.
+//  Copyright © 2017 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
+
+protocol SocketEventCreating {
+    var eventName: SocketEvent { get }
+}
