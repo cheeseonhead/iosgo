2203b68848dfcdc8a47655155add9a5559fd39b3
Jeff Woo <cheeseonhead@gmail.com>
2017-07-02 05:01:35 -0400

    #47 | Connect

--- iosgo/Stores/GameSocket/GameSocket.swift
+++ iosgo/Stores/GameSocket/GameSocket.swift
@@ -9,4 +9,24 @@
 import Foundation
 
 class GameSocket {
+
+    private typealias Models = GameSocketModels
+
+    var socket: SocketManager
+    var gameId: Int
+    var playerId: Int
+
+    required init(socketManager: SocketManager, gameId: Int, playerId: Int) {
+        self.socket = socketManager
+        self.gameId = gameId
+    }
+
+    func connect() {
+        socket.on(GameSocketEventCreator(gameId: gameId, eventType: .move)) { data in
+            print(data[1])
+        }
+
+        let connectData = Models.Connect(chat: true, gameId: gameId, playerId: playerId)
+        socket.emit(event: GameSocketEventCreator(gameId: gameId, eventType: .connect), with: connectData)
+    }
 }
