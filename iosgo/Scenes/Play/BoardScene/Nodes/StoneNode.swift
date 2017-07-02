d11b3405593b0d11fca465d3f9049ecb39369153
Jeff Woo <cheeseonhead@gmail.com>
2017-07-02 16:51:59 -0400

    #47 | Added type to stoneNode

--- iosgo/Scenes/Play/BoardScene/Nodes/StoneNode.swift
+++ iosgo/Scenes/Play/BoardScene/Nodes/StoneNode.swift
@@ -9,4 +9,16 @@
 import SpriteKit
 
 class StoneNode: SKSpriteNode {
+
+    var type: StoneType
+
+    required init(texture: SKTexture?, color: UIColor, size: CGSize, type: StoneType) {
+        super.init(texture: texture, color: color, size: size)
+
+        self.type = type
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
