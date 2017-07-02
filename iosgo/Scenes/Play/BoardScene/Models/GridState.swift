21b7ae2c5b5d79032617105bec8d8f4081de4614
Jeff Woo <cheeseonhead@gmail.com>
2017-07-02 01:51:03 -0400

    #45 | Added a new state object

---
+++ iosgo/Scenes/Play/BoardScene/Models/GridState.swift
@@ -0,0 +1,15 @@
+//
+//  GridState.swift
+//  iosgo
+//
+//  Created by Cheese Onhead on 7/2/17.
+//  Copyright © 2017 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
+
+struct GridState {
+    var blackPrisoners: Int
+    var whitePrisoners: Int
+    var stones: [GridStone]
+}
