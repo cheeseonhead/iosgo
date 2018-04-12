459f59e77f26925bb85c554c806c1d6cf79664bc
Jeffrey Wu <cheeseonhead@gmail.com>
2018-04-12 05:03:00 -0400

    #54 | Implemented a get config method

--- iosgo/Stores/ConfigAPI/ConfigAPI.swift
+++ iosgo/Stores/ConfigAPI/ConfigAPI.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+class ConfigAPI {
+    enum Result<T> {
+        case success(T)
+        case error(ApiErrorType)
+    }
+
+    let apiStore: OGSApiStore
+
+    required init(apiStore: OGSApiStore) {
+        self.apiStore = apiStore
+    }
+
+    func config(completion: @escaping (Result<Config>) -> Void) {
+        let url = "api/v1/ui/config"
+
+        apiStore.request(toUrl: url, method: .GET, parameters: [:]) { code, payload, _ in
+
+            var result = Result<Config>.error(ApiErrorType(statusCode: code))
+
+            switch code {
+            case .ok:
+                if let payload = payload, let config = try? JSONDecoder().decode(Config.self, from: payload) {
+                    result = .success(config)
+                }
+            case .unauthorized:
+                result = .error(.unauthorized)
+            default:
+                break
+            }
+
+            completion(result)
+        }
+    }
+}
