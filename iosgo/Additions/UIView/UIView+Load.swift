ab332085dafdaa50fff4ce5df103068db8109602
Jeffrey Wu <cheeseonhead@gmail.com>
2018-04-19 15:40:06 -0400

    #58 | Added some helping methods

---
+++ iosgo/Additions/UIView/UIView+Load.swift
@@ -0,0 +1,30 @@
+//
+//  UIView+Load.swift
+//  iosgo
+//
+//  Created by Jeffrey Wu on 2018-04-19.
+//  Copyright © 2018 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func setupFromIBDesignable() {
+        let contentView = loadViewFromNib()
+
+        contentView.snp.makeConstraints { make in
+            make.edges.equalTo(self)
+        }
+
+        addSubview(contentView)
+    }
+
+    func loadViewFromNib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: String(describing: type(of: self)), bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+
+        return view
+    }
+}
