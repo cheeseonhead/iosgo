28f5286fbb2a2fd836420b6c338e8607cb01fc74
Jeffrey Wu <cheeseonhead@gmail.com>
2018-04-21 00:09:57 -0400

    #58 | New testing game

--- iosgo/AppScenes/Play/PlayInteractor.swift
+++ iosgo/AppScenes/Play/PlayInteractor.swift
@@ -22,7 +22,7 @@ class PlayInteractor: PlayBusinessLogic, PlayDataStore {
     var playWorker = PlayWorker(gameStore: GameAPI(apiStore: OGSApiStore(sessionController: OGSSessionController.sharedInstance)))
 
     func loadScene(request _: Play.LoadGame.Request) {
-        playWorker.loadGame(id: 3574) { result in
+        playWorker.loadGame(id: 3569) { result in
             switch result {
             case let .success(state):
                 let response = Play.LoadGame.Response(state: state)
