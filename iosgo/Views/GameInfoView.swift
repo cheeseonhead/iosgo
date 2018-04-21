ffb574acc1d85a9c9d4316646ddacdb872e5361d
Jeffrey Wu <cheeseonhead@gmail.com>
2018-04-20 21:26:04 -0400

    #58 | moved the class out

--- iosgo/Views/GameInfoView.swift
+++ iosgo/Views/GameInfoView.swift
@@ -10,43 +10,6 @@ import Foundation
 import SnapKit
 import UIKit
 
-struct PlayerInfoViewModel {
-    let profile: UIImage?
-    let timeStr: String
-    let username: String?
-    let captures: String
-}
-
-// @IBDesignable
-class PlayerInfoView: UIView {
-    @IBOutlet var profileImage: UIImageView!
-    @IBOutlet var timeLabel: UILabel!
-    @IBOutlet var usernameLabel: UILabel!
-    @IBOutlet var capturesLabel: UILabel!
-
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setupFromIBDesignable()
-    }
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupFromIBDesignable()
-    }
-
-    func setModel(_ model: PlayerInfoViewModel) {
-        if let v = model.profile {
-            profileImage.image = v
-        }
-        timeLabel.text = model.timeStr
-        if let v = model.username {
-            usernameLabel.text = v
-        }
-        capturesLabel.text = model.captures
-    }
-}
-
-// @IBDesignable
 class GameInfoView: UIView {
     @IBOutlet var topInfoView: PlayerInfoView!
     @IBOutlet var bottomInfoView: PlayerInfoView!
