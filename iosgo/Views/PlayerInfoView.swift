be4c7bdc0b5c9930d7fa7ed540fde3a01dfc72b3
Jeffrey Wu <cheeseonhead@gmail.com>
2018-04-20 21:26:21 -0400

    #58 | Put the player info view in

--- iosgo/Views/PlayerInfoView.swift
+++ iosgo/Views/PlayerInfoView.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import UIKit
+
+struct PlayerInfoViewModel {
+    let profile: UIImage?
+    let timeStr: String
+    let username: String?
+    let captures: String
+}
+
+class PlayerInfoView: UIView {
+    @IBOutlet var profileImage: UIImageView!
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var usernameLabel: UILabel!
+    @IBOutlet var capturesLabel: UILabel!
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupFromIBDesignable()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupFromIBDesignable()
+    }
+
+    func setModel(_ model: PlayerInfoViewModel) {
+        if let v = model.profile {
+            profileImage.image = v
+        }
+        timeLabel.text = model.timeStr
+        if let v = model.username {
+            usernameLabel.text = v
+        }
+        capturesLabel.text = model.captures
+    }
+}
