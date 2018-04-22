f892d1ce36098be412371fffd858a47b7dd3bf36
Jeff Woo <cheeseonhead@gmail.com>
2018-04-21 22:55:04 -0400

    #56 | Added a codable file

---
+++ iosgo/Models/Game/Game+Codable.swift
@@ -0,0 +1,15 @@
+//
+//  Game+Codable.swift
+//  iosgo
+//
+//  Created by Cheese Onhead on 2018-04-21.
+//  Copyright © 2018 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
+
+extension Game {
+    //    enum CodingKeys: String, CodingKey {
+    //        case <#key#>
+    //    }
+}
