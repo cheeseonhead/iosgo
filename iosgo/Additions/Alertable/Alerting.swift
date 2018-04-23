f5c95c797dc12d2c19be7a45c312a32a5ad076b5
Jeff Woo <cheeseonhead@gmail.com>
2018-04-23 14:13:20 -0400

    #61 | Added protocol

---
+++ iosgo/Additions/Alertable/Alerting.swift
@@ -0,0 +1,21 @@
+//
+//  Alertable.swift
+//  iosgo
+//
+//  Created by Cheese Onhead on 2018-04-23.
+//  Copyright © 2018 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol Alerting {
+    func errorAlert(_ error: Error)
+}
+
+extension Alerting where Self: UIViewController {
+    func errorAlert(_ error: Error) {
+        let controller = UIAlertController.simpleAlert(title: String(describing: type(of: error)), message: error.localizedDescription, actionText: "Ok")
+        present(controller, animated: true, completion: nil)
+    }
+}
