4f7353267e753761efe2382c55a0fab2da589a4c
Jeff Woo <cheeseonhead@gmail.com>
2018-04-25 10:03:05 -0400

    #65 | Toned down the clock controller, we want it to be good at just one thing

--- iosgo/Workers/ClockController.swift
+++ iosgo/Workers/ClockController.swift
@@ -19,43 +19,19 @@ protocol ClockControllerDelegate: class {
 /// It's possible to change the clock type midway, the controller is capable of counting down any clocks.
 class ClockController {
 
-    enum Phase {
-        case waiting, playing
-
-        init(movesCount: Int) {
-            if movesCount == 0 {
-                self = .waiting
-            } else {
-                self = .playing
-            }
-        }
-    }
-
     weak var delegate: ClockControllerDelegate?
 
     private var currentClock: Clock
-    private var gameClock: Clock
-    private var phase: Phase
     private var lastTime = Date()
 
-    init(clock: Clock, phase: Phase) {
-        gameClock = clock
-        currentClock = gameClock
-        self.phase = phase
-        changePhase(phase)
+    init(clock: Clock) {
+        currentClock = clock
     }
 
-    func setClock(_ clock: Clock, phase: Phase) {
-        gameClock = clock
-        currentClock = gameClock
-        self.phase = phase
+    func setClock(_ clock: Clock) {
+        currentClock = clock
 
         lastTime = Date()
-        changePhase(phase)
-    }
-
-    func changePhase(_ phase: Phase) {
-        changeClock(for: phase)
     }
 
     func countDownLoop() {
@@ -84,32 +60,5 @@ private extension ClockController {
 
     func updateClock() {
         delegate?.clockUpdated(currentClock)
-    }
-
-    func changeClock(for phase: Phase) {
-        switch phase {
-        case .waiting:
-            switch currentClock.playingPlayer() {
-            case .black:
-                currentClock.blackTime = pregameWaitingTime(currentClock)
-                currentClock.whiteTime = nil
-            case .white:
-                currentClock.blackTime = nil
-                currentClock.whiteTime = pregameWaitingTime(currentClock)
-            }
-        case .playing:
-            currentClock = gameClock
-        }
-
-        updateClock()
-    }
-
-    func pregameWaitingTime(_ clock: Clock) -> Clock.TimeType {
-        guard let now = clock.now else {
-            return .pregame(Clock.Pregame(thinkingTime: 600))
-        }
-
-        let timeLeft = (clock.expiration - now) / 1000
-        return .pregame(Clock.Pregame(thinkingTime: timeLeft))
     }
 }
