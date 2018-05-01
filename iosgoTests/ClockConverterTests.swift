7132413fde95ce8669fa76c4fef9995761ca8c62
Jeff Woo <cheeseonhead@gmail.com>
2018-05-01 10:22:09 -0400

    #66 | New test file?

---
+++ iosgoTests/ClockConverterTests.swift
@@ -0,0 +1,10 @@
+//
+//  ClockConverterTests.swift
+//  iosgoTests
+//
+//  Created by Cheese Onhead on 2018-05-01.
+//  Copyright © 2018 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
+import Quick
