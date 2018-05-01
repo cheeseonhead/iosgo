f6f95f2cb36378d2815f76402bf932b4116ac20b
Jeff Woo <cheeseonhead@gmail.com>
2018-04-30 23:28:46 -0400

    #66 | Copied some code over

--- iosgo/Engine/Rendering/ClockFormatter.swift
+++ iosgo/Engine/Rendering/ClockFormatter.swift
@@ -29,8 +29,8 @@ class ClockFormatter {
         let seconds = (clock.expiration - now) / 1000
         
         let blackPlaying = clock.playingPlayer() == .black
-        let black = try string(from: time(from: seconds, time: clock.blackTime, playing: blackPlaying))
-        let white = try string(from: time(from: seconds, time: clock.whiteTime, playing: !blackPlaying))
+        let black = try string(from: time(from: seconds, originalTime: clock.blackTime, playing: blackPlaying))
+        let white = try string(from: time(from: seconds, originalTime: clock.whiteTime, playing: !blackPlaying))
 
         return (black, white)
     }
@@ -38,72 +38,20 @@ class ClockFormatter {
 
 // MARK: - Create time from seconds left
 private extension ClockFormatter {
-    
-    func time(from seconds: Double, time: Clock.Time?, playing: Bool) throws -> Clock.Time? {
-        
-        guard let time = time else {
-            return nil
-        }
-        
-        switch type {
-        case .byoyomi:
-            return try byoyomiTime(from: seconds, time: time)
-        case .pregame:
-            if !playing {
-                return nil
-            }
-            fallthrough
-        default:
-            return regularTime(from: seconds)
-        }
-    }
-    
-    func regularTime(from seconds: Double) -> Clock.Time {
-        return Clock.Time(thinkingTime: seconds)
-    }
-    
-    func byoyomiTime(from seconds: Double, time: Clock.Time) throws -> Clock.Time {
-        guard let periods = time.periods, let periodTime = time.periodTime else {
-            throw ParseError.propertiesMissingValue([\Clock.Time.periods, \Clock.Time.periodTime])
-        }
-        
-        var periodsLeft = 0
-        var timeLeft = seconds
-        while periodsLeft < periods && timeLeft > periodTime {
-            periodsLeft += 1
-            timeLeft -= periodTime
-        }
-        
-        return Clock.Time(thinkingTime: timeLeft, periods: periodsLeft, periodTime: periodTime)
-    }
 }
 
 // MARK: - Format Clock
 
 private extension ClockFormatter {
-    func pregameFormat(_ clock: Clock) throws -> (black: String, white: String) {
-        switch clock.playingPlayer() {
-        case .black:
-            return (try pregameString(clock), NSLocalizedString("Waiting...", comment: ""))
-        case .white:
-            return (NSLocalizedString("Waiting...", comment: ""), try pregameString(clock))
-        }
-    }
+//    func pregameFormat(_ clock: Clock) throws -> (black: String, white: String) {
+//        switch clock.playingPlayer() {
+//        case .black:
+//            return (try pregameString(clock), NSLocalizedString("Waiting...", comment: ""))
+//        case .white:
+//            return (NSLocalizedString("Waiting...", comment: ""), try pregameString(clock))
+//        }
+//    }
 
-    func pregameString(_ clock: Clock) throws -> String {
-        guard let duration = clock.secondsUntilExpiration() else {
-            throw ParseError.propertiesMissingValue([\Clock.now])
-        }
-
-        let formatter = DateComponentsFormatter()
-        formatter.allowedUnits = [.day, .hour, .minute, .second]
-        formatter.unitsStyle = .abbreviated
-
-        let format = NSLocalizedString("Time to make first move: %@", comment: "")
-        let timeString = formatter.string(from: duration)!
-
-        return String(format: format, timeString)
-    }
 }
 
 // MARK: - Format Time
@@ -118,7 +66,7 @@ private extension ClockFormatter {
         case .byoyomi:
             return try byoyomiFormat(time)
         case .pregame:
-            return "Waiting..."
+            return try pregameFormat(time)
         default:
             return "Not yet implemented"
         }
@@ -137,6 +85,17 @@ private extension ClockFormatter {
         let pdTime = formatter.string(from: periodTime)!
 
         return String(format: format, mainTime, pdTime, periods)
+    }
+    
+    func pregameFormat(_ time: Clock.Time) throws -> String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.day, .hour, .minute, .second]
+        formatter.unitsStyle = .abbreviated
+        
+        let format = NSLocalizedString("Time to make first move: %@", comment: "")
+        let timeString = formatter.string(from: time.thinkingTime)!
+        
+        return String(format: format, timeString)
     }
 
     func getFormatter() -> DateComponentsFormatter {
--- iosgo/Workers/TimeConverter.swift
+++ iosgo/Workers/TimeConverter.swift
@@ -7,3 +7,50 @@
 //
 
 import Foundation
+
+/// Given a **Clock**, **TimeConverter** will return a new **Clock** object
+/// corresponding to the actual situation of the players' times.
+///
+/// This is due to the **Clock** object given by the server not accurately
+/// calculating the remaining times. So we have to derive it from **now** and
+/// **expiration** properties.
+class TimeConverter {
+    let type: TimeControlType
+    
+    required init(type: TimeControlType) {
+        self.type = type
+    }
+    
+    func time(from seconds: Double, originalTime: Clock.Time?) throws -> Clock.Time? {
+        
+        guard let time = originalTime else {
+            return nil
+        }
+        
+        switch type {
+        case .byoyomi:
+            return try byoyomiTime(from: seconds, time: time)
+        default:
+            return regularTime(from: seconds)
+        }
+    }
+    
+    func regularTime(from seconds: Double) -> Clock.Time {
+        return Clock.Time(thinkingTime: seconds)
+    }
+    
+    func byoyomiTime(from seconds: Double, time: Clock.Time) throws -> Clock.Time {
+        guard let periods = time.periods, let periodTime = time.periodTime else {
+            throw ParseError.propertiesMissingValue([\Clock.Time.periods, \Clock.Time.periodTime])
+        }
+        
+        var periodsLeft = 0
+        var timeLeft = seconds
+        while periodsLeft < periods && timeLeft > periodTime {
+            periodsLeft += 1
+            timeLeft -= periodTime
+        }
+        
+        return Clock.Time(thinkingTime: timeLeft, periods: periodsLeft, periodTime: periodTime)
+    }
+}
