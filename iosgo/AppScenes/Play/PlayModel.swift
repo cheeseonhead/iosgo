077a6773f1a23a8ea3a0a58415650218e0c4db12
Cheeseonhead <cheeseonhead@gmail.com>
2018-05-26 01:20:08 -0400

    #68 | Added a play model

---
+++ iosgo/AppScenes/Play/PlayModel.swift
@@ -0,0 +1,12 @@
+//
+//  PlayModel.swift
+//  iosgo
+//
+//  Created by Jeffrey Wu on 2018-05-26.
+//  Copyright © 2018 Cheeseonhead. All rights reserved.
+//
+
+import Foundation
+
+struct PlayModel {
+}
